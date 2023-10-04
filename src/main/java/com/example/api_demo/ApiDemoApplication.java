48d9361354de9d635a8192256fbc9de0fa2b0ea4
YUSIEUN1013 <db4067ww@gmail.com>
2023-10-04 16:01:59 +0900

    test

--- src/main/java/com/example/api_demo/ApiDemoApplication.java
+++ src/main/java/com/example/api_demo/ApiDemoApplication.java
@@ -10,11 +10,9 @@ public class ApiDemoApplication {
 
         SpringApplication.run(ApiDemoApplication.class, args);
 
-        while (true){
+
             System.out.println("실행 완료");
 
-            Thread.sleep(100000);
-        }
 
     }
 
